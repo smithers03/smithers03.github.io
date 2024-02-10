ffe9749cc6ac8ee8562e26eee1c7f8bcf1da6f82
Ellie Smith <smithe17@tcd.ie>
2024-02-10 01:31:11 +0000

    making a background color

---
+++ main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+function App() {
+  return (
+    <div>
+      {/* Your website content goes here */}
+    </div>
+  );
+}
+
+export default App;

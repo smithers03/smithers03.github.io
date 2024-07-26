d4c1d31eaed4a79f4758d70135a5ffcaa1c46ad5
Ellie Smith <smithe17@tcd.ie>
2024-07-26 02:25:06 +0100

    trying to get this to work and display right

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import MyComponent from './Message';
+import React from 'react';
+import './index.css';
 
-function App() {
-  return <div><MyComponent></MyComponent></div>;
-}
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="title">Hello there friends</h1>
+    </div>
+  );
+};
 
 export default App;
+
